8b43e4d25fce44e122a08d6551bcc9cb009b129a
Chandrakant <cks3383@gmail.com>
2017-10-03 05:45:10 +0530

    Fixed stage3

--- base-system/stage3.sh
+++ base-system/stage3.sh
@@ -11,20 +11,21 @@ mkdir -pv $LFS/{dev,proc,sys,run}
 
 # Create /dev/console and /dev/null if not done already
 
-if [ ! -e /dev/console ] || [ ! -e /dev/null ]
-mknod -m 600 $LFS/dev/console c 5 1
-mknod -m 666 $LFS/dev/null c 1 3
+if [ ! -e /dev/console ] || [ ! -e /dev/null ]; then
+	
+	mknod -m 600 $LFS/dev/console c 5 1
+	mknod -m 666 $LFS/dev/null c 1 3
 
-mount -v --bind /dev $LFS/dev
+	mount -v --bind /dev $LFS/dev
 
-mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
-mount -vt proc proc $LFS/proc
-mount -vt sysfs sysfs $LFS/sys
-mount -vt tmpfs tmpfs $LFS/run
+	mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
+	mount -vt proc proc $LFS/proc
+	mount -vt sysfs sysfs $LFS/sys
+	mount -vt tmpfs tmpfs $LFS/run
 
-if [ -h $LFS/dev/shm ]; then
-  mkdir -pv $LFS/$(readlink $LFS/dev/shm)
-fi
+	if [ -h $LFS/dev/shm ]; then
+	  mkdir -pv $LFS/$(readlink $LFS/dev/shm)
+	fi
 
 fi
 
