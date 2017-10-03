a6a1ffce88ca03cb0d938d6bb1978cc08f108896
chandrakant <cks3383@gmail.com>
2017-10-03 10:34:13 +0530

    Fixed efibootvar and efibootmgr and updated their versions

--- base-system/extras/006-efivar.sh
+++ base-system/extras/006-efivar.sh
@@ -9,7 +9,7 @@ export MAKEFLAGS="-j `nproc`"
 SOURCE_DIR="/sources"
 LOGFILE="/sources/build-log"
 STEPNAME="006-efivar.sh"
-TARBALL="efivar-0.23.tar.bz2"
+TARBALL="efivar-31.tar.bz2"
 
 if ! grep "$STEPNAME" $LOGFILE &> /dev/null
 then
@@ -23,11 +23,7 @@ then
 	cd $DIRECTORY
 fi
 
-patch -Np1 -i ../0.21-nvme_ioctl.h.patch
-sed 's|-O0|-Os|g' -i Make.defaults
-sed 's|-rpath=$(TOPDIR)/src/|-rpath=$(libdir)|g' \
-     -i src/test/Makefile
-CFLAGS="-Wno-error=deprecated-declarations" make libdir="/usr/lib/" bindir="/usr/bin/" \
+make libdir="/usr/lib/" bindir="/usr/bin/" \
 	mandir="/usr/share/man/"     \
 	includedir="/usr/include/" V=1 -j1
 pushd src/test
