7882c2d86ff70bda43dacb5ccf54d26632848547
chandrakant <cks3383@gmail.com>
2017-10-05 08:13:41 +0530

    Added python-modules#bsddb3.sh

---
+++ applications/python-modules#bsddb3.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+NAME="python-modules#bsddb3"
+VERSION="6.1.1"
+
+#REQ:db
+
+cd $SOURCE_DIR
+URL=https://pypi.python.org/packages/95/1c/e8fb33007192f30b9a7276560c3c16499ab2a2c08abc59141b84e1afd3a9/bsddb3-6.1.1.tar.gz
+wget -nc $URL
+
+TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq`
+
+tar xf $TARBALL
+cd $DIRECTORY
+
+python setup.py build
+sudo python setup.py install
+
+#python3 setup.py build
+#sudo python3 setup.py install
+
+cd $SOURCE_DIR
+cleanup "$NAME" "$DIRECTORY"
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
