57c842279571f436d72294d356693f39dd7ce161
chandrakant <cks3383@gmail.com>
2017-10-05 20:14:41 +0530

    Fixed and upped version for packagekit

--- applications/packagekit.sh
+++ applications/packagekit.sh
@@ -5,17 +5,17 @@ set +h
 . /etc/alps/alps.conf
 . /var/lib/alps/functions
 
-NAME="PackageKit"
-VERSION="1.1.1"
+NAME="packagekit"
+VERSION="1.1.7"
 
 #REQ:bash-completion
 
 cd $SOURCE_DIR
 
-URL=https://www.freedesktop.org/software/PackageKit/releases/PackageKit-1.1.1.tar.xz
+URL=https://www.freedesktop.org/software/PackageKit/releases/PackageKit-1.1.7.tar.xz
 wget -nc $URL
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
-DIRECTORY=`tar -tf $TARBALL | sed -e 's@/.*@@' | uniq `
+DIRECTORY=`tar -tf $TARBALL | cut -d/ -f1 | uniq`
 
 tar -xf $TARBALL
 
