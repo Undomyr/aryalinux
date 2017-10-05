22085209b376431f151312ed430e15247c99334f
chandrakant <cks3383@gmail.com>
2017-10-05 08:21:18 +0530

    Fixed python-modules#bsddb3.sh

--- applications/python-modules#bsddb3.sh
+++ applications/python-modules#bsddb3.sh
@@ -14,6 +14,7 @@ VERSION="6.1.1"
 cd $SOURCE_DIR
 URL=https://pypi.python.org/packages/95/1c/e8fb33007192f30b9a7276560c3c16499ab2a2c08abc59141b84e1afd3a9/bsddb3-6.1.1.tar.gz
 wget -nc $URL
+wget -nc https://raw.githubusercontent.com/FluidIdeas/patches/2017.09/bsddb3-6.1.1-assertion-error.patch
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq`
@@ -21,11 +22,12 @@ DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq`
 tar xf $TARBALL
 cd $DIRECTORY
 
+patch -Np1 -i ../bsddb3-6.1.1-assertion-error.patch
 python setup.py --berkeley-db=/usr build
 sudo python setup.py install
 
-#python3 setup.py build
-#sudo python3 setup.py install
+python3 setup.py build
+sudo python3 setup.py install
 
 cd $SOURCE_DIR
 cleanup "$NAME" "$DIRECTORY"
