e3902737bdc111969585b8a46192ebde9208bed3
Chandrakant <cks3383@gmail.com>
2017-10-20 04:53:18 +0530

    Fixed stage3. Virtual filesystems were not getting mounted when there was no error

--- base-system/stage3.sh
+++ base-system/stage3.sh
@@ -11,20 +11,31 @@ mkdir -pv $LFS/{dev,proc,sys,run}
 
 # Create /dev/console and /dev/null if not done already
 
-if [ ! -e /dev/console ] || [ ! -e /dev/null ]; then
+if [ ! -e /dev/console ]; then
 	
 	mknod -m 600 $LFS/dev/console c 5 1
-	mknod -m 666 $LFS/dev/null c 1 3
-
-	mount -v --bind /dev $LFS/dev
-
-	mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
-	mount -vt proc proc $LFS/proc
-	mount -vt sysfs sysfs $LFS/sys
-	mount -vt tmpfs tmpfs $LFS/run
-
-	mount -vt tmpfs tmpfs $LFS/dev/shm
 fi
+if [ ! -e /dev/null ]; then
+	mknod -m 666 $LFS/dev/null c 1 3
+fi
+
+set +e
+umount $LFS/dev/pts
+umount $LFS/dev/shm
+umount $LFS/dev
+umount $LFS/sys
+umount $LFS/proc
+umount $LFS/run
+set -e
+
+mount -v --bind /dev $LFS/dev
+
+mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
+mount -vt proc proc $LFS/proc
+mount -vt sysfs sysfs $LFS/sys
+mount -vt tmpfs tmpfs $LFS/run
+
+mount -vt tmpfs tmpfs $LFS/dev/shm
 
 # Building Final System
 
