a5e5a555e1154ac748492326466a612b230d3d96
Chandrakant <chandrakantsingh@protonmail.com>
2017-11-23 13:56:58 +0530

    Updated download location

--- base-system/additional-downloads.sh
+++ base-system/additional-downloads.sh
@@ -39,7 +39,7 @@ unzip master.zip
 tar -cJvf dracut-master.tar.xz dracut-master
 rm -r dracut-master
 rm -r master.zip
-wget -nc ftp://sources.redhat.com/pub/lvm2/releases/LVM2.2.02.155.tgz
+wget -nc https://www.mirrorservice.org/sites/sourceware.org/pub/lvm2/releases/LVM2.2.02.155.tgz
 wget -nc https://sourceforge.net/projects/aryalinux-bin/files/releases/$VERSION/aufs-4.14.tar.gz
 wget -nc https://sourceforge.net/projects/aryalinux-bin/files/releases/$VERSION/aufs4-base.patch
 wget -nc https://sourceforge.net/projects/aryalinux-bin/files/releases/$VERSION/aufs4-kbuild.patch
