72dea4716ea5a32fc2eb90d7643b81139f8d046b
Chandrakant Singh <chandrakant.singh@tavant.com>
2017-12-26 12:53:00 +0530

    Fixed kernel.sh not to use dracut

--- base-system/kernel.sh
+++ base-system/kernel.sh
@@ -207,7 +207,9 @@ make install
 cd /sources
 rm -rf $FIRMWARE_DIR
 
-dracut -f /boot/initrd.img-$LINUX_VERSION `ls /lib/modules`
+# dracut -f /boot/initrd.img-$LINUX_VERSION `ls /lib/modules`
+mkinitramfs `ls /lib/modules`
+mv initrd.img* /boot/
 
 echo "$STEPNAME" | tee -a $LOGFILE
 
