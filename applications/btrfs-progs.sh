3af29d9c71f7af06ade5d06cfb331dc733a0257b
Chandrakant Singh <chandrakant.singh@tavant.com>
2017-12-28 03:47:46 +0530

    Upped version of dosfstools and fixed btrfs-progs

--- applications/btrfs-progs.sh
+++ applications/btrfs-progs.sh
@@ -46,23 +46,6 @@ sed -i '1,100 s/\.gz//g' Documentation/Makefile.in &&
 make "-j`nproc`" || make
 
 
-mv tests/fuzz-tests/003-multi-check-unmounted/test.sh{,.broken}    &&
-mv tests/fuzz-tests/004-simple-dump-tree/test.sh{,.broken}         &&
-mv tests/fuzz-tests/007-simple-super-recover/test.sh{,.broken}     &&
-mv tests/fuzz-tests/009-simple-zero-log/test.sh{,.broken}          &&
-mv tests/misc-tests/019-receive-clones-on-munted-subvol/test.sh{,.broken}
-
-
-pushd tests
-   ./fsck-tests.sh
-   ./mkfs-tests.sh
-   ./convert-tests.sh
-   ./misc-tests.sh
-   ./cli-tests.sh
-   ./fuzz-tests.sh
-popd
-
-
 
 sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
 make install &&
--- applications/dosfstools.sh
+++ applications/dosfstools.sh
@@ -1,29 +1,59 @@
 #!/bin/bash
+
 set -e
 set +h
 
 . /etc/alps/alps.conf
 . /var/lib/alps/functions
 
+SOURCE_ONLY=n
+DESCRIPTION="br3ak The dosfstools package containsbr3ak various utilities for use with the FAT family of file systems.br3ak"
+SECTION="postlfs"
+VERSION=4.1
 NAME="dosfstools"
-VERSION="3.0.26"
+
+
 
 cd $SOURCE_DIR
 
-URL=http://daniel-baumann.ch/files/software/dosfstools/dosfstools-3.0.26.tar.xz
-wget -nc $URL || wget -nc http://pkgs.fedoraproject.org/lookaside/pkgs/dosfstools/dosfstools-3.0.26.tar.xz/45012f5f56f2aae3afcd62120b9e5a08/dosfstools-3.0.26.tar.xz
+URL=https://github.com/dosfstools/dosfstools/releases/download/v4.1/dosfstools-4.1.tar.xz
+
+if [ ! -z $URL ]
+then
+wget -nc https://github.com/dosfstools/dosfstools/releases/download/v4.1/dosfstools-4.1.tar.xz || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/dosfstools/dosfstools-4.1.tar.xz || wget -nc http://mirrors-ru.go-parts.com/blfs/conglomeration/dosfstools/dosfstools-4.1.tar.xz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/dosfstools/dosfstools-4.1.tar.xz || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/dosfstools/dosfstools-4.1.tar.xz || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/dosfstools/dosfstools-4.1.tar.xz || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/dosfstools/dosfstools-4.1.tar.xz
+
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
-DIRECTORY=`tar -tf $TARBALL | sed -e 's@/.*@@' | uniq `
-
-tar -xf $TARBALL
-
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
 cd $DIRECTORY
+fi
 
-sed -i "s@PREFIX = /usr/local@PREFIX = /usr@g" Makefile
-make
-sudo make install
+whoami > /tmp/currentuser
 
-cd $SOURCE_DIR
-cleanup "$NAME" "$DIRECTORY"
+./configure --prefix=/               \
+            --enable-compat-symlinks \
+            --mandir=/usr/share/man  \
+            --docdir=/usr/share/doc/dosfstools-4.1 &&
+make "-j`nproc`" || make
+
+
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+make install
+
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
+
+
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
 
 register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
