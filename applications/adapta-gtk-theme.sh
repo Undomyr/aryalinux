691874116be4d60c3a271b315cb7033c0e4c8744
Chandrakant <cks3383@gmail.com>
2018-01-18 20:18:47 +0530

    Fixed adapta-gtk-theme

--- applications/adapta-gtk-theme.sh
+++ applications/adapta-gtk-theme.sh
@@ -16,20 +16,12 @@ VERSION="3.93.0.56"
 
 cd $SOURCE_DIR
 
-URL=https://sourceforge.net/projects/aryalinux-bin/files/releases/1.0/adapta-gtk-theme-3.93.0.56.tar.xz
-TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+URL=https://sourceforge.net/projects/aryalinux-bin/files/releases/1.0/adapta-gtk-theme.tar.xz
 
 wget -nc $URL
-DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq)
 
-tar xf $TARBALL
-cd $DIRECTORY
-
-./autogen.sh --prefix=/usr
-make -j$(nproc)
-sudo make install
+sudo tar xf adapta-gtk-theme.tar.xz -C /usr/share/themes/
 
 cd $SOURCE_DIR
-rm -rf $DIRECTORY
 
 register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
