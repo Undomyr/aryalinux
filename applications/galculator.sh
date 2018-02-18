1d845010d2c60d50adc1558aed51778863cef45f
chandrakant <cks3383@gmail.com>
2018-02-19 03:02:43 +0530

    Bumped galculator version

--- applications/galculator.sh
+++ applications/galculator.sh
@@ -8,11 +8,11 @@ set +h
 SOURCE_ONLY=n
 NAME="galculator"
 DESCRIPTION="A simple gtk based calculator application"
-VERSION=2.1.3
+VERSION=2.1.4
 
 cd $SOURCE_DIR
 
-URL=http://pkgs.fedoraproject.org/repo/pkgs/galculator/galculator-2.1.3.tar.bz2/a7a333cc4c321507434fe3f8e48fcd0a/galculator-2.1.3.tar.bz2
+URL=http://galculator.mnim.org/downloads/galculator-2.1.4.tar.bz2
 wget -nc $URL
 TARBALL=galculator-2.1.3.tar.bz2
 DIRECTORY=`tar -tf $TARBALL | sed -e 's@/.*@@' | uniq `
