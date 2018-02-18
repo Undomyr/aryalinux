55c4c4f61c4d1429c8d95b0ba76332584da34b78
chandrakant <cks3383@gmail.com>
2018-02-19 03:06:57 +0530

    modified:   applications/xfce4-screenshooter.sh

--- applications/xfce4-screenshooter.sh
+++ applications/xfce4-screenshooter.sh
@@ -23,7 +23,7 @@ tar -xf $TARBALL
 
 cd $DIRECTORY
 
-./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-static &&
+./autogen.sh --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-static &&
 make
 sudo make install
 
