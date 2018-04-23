6e8f4cb85ec408c1978b1ccc3ca6a1bbef35632e
Chandrakant <cks3383@gmail.com>
2018-04-23 19:43:43 +0530

    Updated linux kernel version

--- base-system/additional-downloads.sh
+++ base-system/additional-downloads.sh
@@ -36,7 +36,7 @@ wget -nc http://gondor.apana.org.au/~herbert/dash/files/dash-0.5.9.1.tar.gz
 wget -nc https://sourceware.org/ftp/lvm2/releases/LVM2.2.02.171.tgz
 wget -nc http://anduin.linuxfromscratch.org/BLFS/other/make-ca.sh-20170514
 wget -nc https://sourceware.org/ftp/elfutils/0.170/elfutils-0.170.tar.bz2
-wget -nc https://sourceforge.net/projects/aryalinux-bin/files/releases/$VERSION/aufs-4.15.tar.gz
+wget -nc https://github.com/FluidIdeas/patches/raw/1.1/aufs-4.16.tar.gz
 wget -nc https://raw.githubusercontent.com/FluidIdeas/patches/$VERSION/aufs4-base.patch
 wget -nc https://raw.githubusercontent.com/FluidIdeas/patches/$VERSION/aufs4-kbuild.patch
 wget -nc https://raw.githubusercontent.com/FluidIdeas/patches/$VERSION/aufs4-mmap.patch
