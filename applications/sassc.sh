fe6c1b1f5f66e0b0790e2ded2cf9e7c9e3a5ac78
Chandrakant Singh <aryalinux11@gmail.com>
2018-04-29 16:45:57 +0530

    Updated sassc

--- applications/sassc.sh
+++ applications/sassc.sh
@@ -6,28 +6,70 @@ set +h
 . /etc/alps/alps.conf
 . /var/lib/alps/functions
 
-NAME=sassc
-DESCRIPTION="libsass command line driver"
-VERSION=3.4.8
+SOURCE_ONLY=n
+DESCRIPTION="br3ak SassC is a wrapper around libsass, a CSS pre-processor language.br3ak"
+SECTION="gnome"
+VERSION=3.5.0
+NAME="sassc"
 
-URL=https://github.com/sass/sassc/releases/download/3.4.8/sassc-3.4.8.tar.gz
+
 
 cd $SOURCE_DIR
 
-wget -nc $URL
-TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
-DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq)
+URL=https://github.com/sass/sassc/archive/3.5.0/sassc-3.5.0.tar.gz
 
-tar xf $TARBALL
+if [ ! -z $URL ]
+then
+wget -nc https://github.com/sass/sassc/archive/3.5.0/sassc-3.5.0.tar.gz || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/sassc/sassc-3.5.0.tar.gz || wget -nc http://mirrors-ru.go-parts.com/blfs/conglomeration/sassc/sassc-3.5.0.tar.gz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/sassc/sassc-3.5.0.tar.gz || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/sassc/sassc-3.5.0.tar.gz || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/sassc/sassc-3.5.0.tar.gz || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/sassc/sassc-3.5.0.tar.gz
+wget -nc https://github.com/sass/libsass/archive/3.5.2/libsass-3.5.2.tar.gz
+
+TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
 cd $DIRECTORY
+fi
 
-./autoreconf --force --install
-./configure --prefix=/usr --enable-shared &&
-make
-sudo make install
+whoami > /tmp/currentuser
 
-cd $SOURCE_DIR
-sudo rm -rf libsass-3.4.9
+autoreconf -fi &&
+./configure --prefix=/usr --disable-static &&
+make "-j`nproc`" || make
 
-echo "sassc=>$(date)" | sudo tee -a /etc/alps/installed-list
-echo "sassc:$VERSION" | sudo tee -a /etc/alps/versions
+
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+make install
+
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
+
+
+tar -xf ../sassc-3.5.0.tar.gz &&
+cd sassc-3.5.0 &&
+autoreconf -fi &&
+./configure --prefix=/usr &&
+make "-j`nproc`" || make
+
+
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+make install
+
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo bash -e ./rootscript.sh
+sudo rm rootscript.sh
+
+
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
