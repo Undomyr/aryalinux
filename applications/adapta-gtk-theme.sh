a409f655bf118506dceb777d0855698828743b89
Chandrakant <aryalinux11@gmail.com>
2018-06-03 07:20:28 +0530

    Fixed adapta-gtk-theme

--- applications/adapta-gtk-theme.sh
+++ applications/adapta-gtk-theme.sh
@@ -9,28 +9,21 @@ set +h
 SOURCE_ONLY=n
 NAME="adapta-gtk-theme"
 DESCRIPTION="Adapta GTK theme"
-VERSION="3.93.0.149"
+VERSION="3.93.1.18"
 
 #REQ:gtk2
 #REQ:gtk3
 #REQ:sassc
 
-URL=https://github.com/adapta-project/adapta-gtk-theme/archive/3.93.0.149.tar.gz
-TARBALL="adapta-gtk-theme-$VERSION.tar.gz"
+URL=https://sourceforge.net/projects/aryalinux-bin/files/artifacts/adapta-gtk-theme.tar.xz
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
 
 cd $SOURCE_DIR
 
-wget -c $URL -O $TARBALL
-DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq)
+wget -nc $URL
 
-tar xf $TARBALL
-cd $DIRECTORY
-./autogen.sh --prefix=/usr
-./configure --prefix=/usr --enable-shared &&
-make
-sudo make install
+sudo tar xf $TARBALL -C /
 
 cd $SOURCE_DIR
-sudo rm -rf $DIRECTORY
 
 register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
